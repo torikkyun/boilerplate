dfa29d22f7b37c902a88e99cba1f894fda2e8d9a
torikkyun <nduc42176@gmail.com>
2025-08-02 22:04:16 +0700

    Thêm các common

--- prisma-mongo/src/app.module.ts
+++ prisma-mongo/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './models/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        process.env.NODE_ENV === 'production'
+          ? '.env'
+          : '.env.development.local',
+      ],
+      expandVariables: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
