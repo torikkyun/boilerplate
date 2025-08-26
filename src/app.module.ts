0506709258f691c776d69d91a76723728315ce05
torikkyun <nduc42176@gmail.com>
2025-08-27 00:50:49 +0700

    Cập nhật common

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { AuthModule } from './core/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
