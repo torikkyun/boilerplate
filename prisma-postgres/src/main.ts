ff6acead991260d2d8a3bb9aff8c7d81b09b6fa4
torikkyun <nduc42176@gmail.com>
2025-09-07 21:26:43 +0700

    bỏ fastify

--- prisma-postgres/src/main.ts
+++ prisma-postgres/src/main.ts
@@ -5,16 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@common/filters/http-exception.filter';
 import { TransformInterceptor } from '@common/interceptors/transform.interceptor';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   app.enableCors();
@@ -46,7 +39,7 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, config),
   );
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 
 bootstrap().catch((error) => {
